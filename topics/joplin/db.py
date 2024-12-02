163c41580bb441668cc66db53e805b3b15c078a9
Hai Vu <haivu2004@gmail.com>
2024-12-01 19:57:01 -0800

    Add search

--- topics/joplin/db.py
+++ topics/joplin/db.py
@@ -15,27 +15,9 @@ logging.basicConfig(
 
 def connect():
     root = pathlib.Path("~/.config/joplin-desktop").expanduser()
-    assert root.is_dir()
-
     db_path = root / "database.sqlite"
     logging.debug("db_path=%r", db_path)
-    assert db_path.exists()
-    assert not db_path.is_dir()
+    if not (db_path.exists() and not db_path.is_dir()):
+        raise RuntimeError(f"Database does not exist: {db_path}")
 
     return sqlite3.connect(f"file:{db_path}?mode=ro")
-
-
-# c = connect()
-# # query = "select id,title from notes"
-# # for row in c.execute(query):
-# #     print(row)
-
-# # Search for text
-# target = "mouse"
-# target = f"%{target}%"
-# for row in c.execute("select title from notes where body like ?", (target,)):
-#     title = row[0]
-#     # id_, title, body = row
-#     # print("---")
-#     print(title)
-#     # print(body)
--- topics/joplin/note_search.py
+++ topics/joplin/note_search.py
@@ -1,17 +1,30 @@
 """
 Search Notes
 """
+
 import argparse
+import logging
+import os
+
 import db
+
+logging.basicConfig(
+    level=os.getenv("LOGLEVEL", "WARNING"),
+    format="%(levelname)-12s | %(message)s",
+)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("terms", nargs="+")
 options = parser.parse_args()
-print(options)
 
-terms = " or ".join("term like %?%" for term in options.terms)
-print(terms)
+terms = " or ".join("body like ?" for term in options.terms)
+sql = f"select title from notes where {terms}"
+logging.debug("sql=%r", sql)
+logging.debug("terms=%r", options.terms)
 
 with db.connect() as connection:
-    for (title,) in connection.execute(f"select title from notes where {terms}", options.terms):
+    for (title,) in connection.execute(
+        f"select title from notes where {terms}",
+        [f"%{term}%" for term in options.terms],
+    ):
         print(title)
