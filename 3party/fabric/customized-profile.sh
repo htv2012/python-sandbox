6aaad8fdcc9f8b1b61da490f726c2142cac02ae3
Hai Vu <haivu2004@gmail.com>
2025-01-07 07:16:26 -0800

    Remove dated customized-profile.sh

--- 3party/fabric/customized-profile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -d "/etc/nms/modules/adm/templates/usecases" ]
-then
-    alias cdu='cd /etc/nms/modules/adm/templates/usecases'
-    cd /etc/nms/modules/adm/templates/usecases || true
-fi
-
-alias cdt='cd /tmp'
