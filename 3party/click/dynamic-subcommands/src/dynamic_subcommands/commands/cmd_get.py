769c222739daaa5927f504541563e03b66292bec
Hai Vu <haivu2004@gmail.com>
2025-02-07 12:31:11 -0800

    Add doc dynamic-subcommands

--- 3party/click/dynamic-subcommands/src/dynamic_subcommands/commands/cmd_get.py
+++ 3party/click/dynamic-subcommands/src/dynamic_subcommands/commands/cmd_get.py
@@ -4,4 +4,9 @@ import click
 @click.command()
 @click.pass_context
 def main(ctx: click.Context):
+    """
+    Extract data from the cloud
+    
+    Retrieve data from the server and display it
+    """
     ctx.obj.api.get()
--- 3party/click/dynamic-subcommands/src/dynamic_subcommands/commands/cmd_put.py
+++ 3party/click/dynamic-subcommands/src/dynamic_subcommands/commands/cmd_put.py
@@ -4,4 +4,8 @@ import click
 @click.command()
 @click.pass_context
 def main(ctx: click.Context):
+    """Send data to the cloud
+    
+    Write a bunch of data to the server
+    """
     ctx.obj.api.put()
