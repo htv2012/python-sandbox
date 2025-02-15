e63f2e5c6067b7041568ceaef3dcc6b2cd93a964
Hai Vu <haivu2004@gmail.com>
2025-02-15 11:07:53 -0800

    Add Progress class to avoid hacking into tqdm's internal

--- 3party/fabric/upload_to_remote_with_progress.py
+++ 3party/fabric/upload_to_remote_with_progress.py
@@ -9,8 +9,26 @@ import fabric
 import tqdm
 
 
+class Progress:
+    def __init__(self, bar):
+        self.sofar = 0
+        self.bar = bar
+
+    def update(self, sofar, _):
+        delta = sofar - self.sofar
+        self.sofar = sofar
+        self.bar.update(delta)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exception_type, exception_instance, exception_traceback):
+        self.bar.close()
+
+
 def main():
     hostname = "ssh-sandbox"
+    local_path = "/tmp/big"
 
     config = fabric.Config()
     if hostname not in config.base_ssh_config.get_hostnames():
@@ -19,12 +37,14 @@ def main():
     with contextlib.ExitStack() as stack:
         conn = stack.enter_context(fabric.Connection(host=hostname))
         sftp = stack.enter_context(conn.sftp())
-        conn.local("truncate -s 1GB /tmp/big")
-        pbar = stack.enter_context(tqdm.tqdm(total=os.stat("/tmp/big").st_size))
+        conn.local(f"truncate -s 1GB {local_path}")  # Create large local file
+        progress = stack.enter_context(
+            Progress(tqdm.tqdm(total=os.stat(local_path).st_size, unit="bytes"))
+        )
         sftp.put(
             "/tmp/big",
             "/tmp/big",
-            callback=lambda sofar, total: pbar.update(sofar - pbar.n),
+            callback=progress.update,
         )
 
         # Clean up
