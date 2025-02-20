4c4f8864a4af912e4bfdaa00a97d2cbb89bf9b03
Hai Vu <haivu2004@gmail.com>
2025-02-20 09:40:35 -0800

    update curses_tryout.py

--- stdlib/curses/curses_tryout.py
+++ stdlib/curses/curses_tryout.py
@@ -4,12 +4,12 @@ import curses
 
 myscreen = curses.initscr()
 
-# myscreen.border(0)
-# myscreen.addstr(12, 25, "Python curses in action!")
+#myscreen.border(0)
+myscreen.addstr(0, 0, "Python curses in action!")
 myscreen.refresh()
 
 
-w = curses.newwin(20, 0, 1, 1)
+w = curses.newwin(20, 0, 2, 2)
 w.border(0)
 w.addstr(5, 5, 'PY is cool')
 w.refresh()
