e0736981af7b6e1662be609ef69d3aa0dfb8b236
Hai Vu <haivu2004@gmail.com>
2025-03-28 11:43:36 -0700

    Handle relative imports, broken symlink

--- stdlib/ast/show_imports.py
+++ stdlib/ast/show_imports.py
@@ -7,7 +7,14 @@ from collections import defaultdict
 
 
 def find_imports(path: Path, found: dict):
-    content = path.read_text()
+    if '.venv' in path.parts:
+        return
+
+    try:
+        content = path.read_text()
+    except FileNotFoundError:
+        # Possible cause: broken symlink
+        return
     try:
         root = ast.parse(content, path)
     except SyntaxError:
@@ -18,7 +25,19 @@ def find_imports(path: Path, found: dict):
             for alias in entity.names:
                 found[alias.name].add((path, entity.lineno))
         elif isinstance(entity, ast.ImportFrom):
-            found[entity.module].add((path, entity.lineno))
+            if entity.level == 0:
+                # Absolute import
+                found[entity.module].add((path, entity.lineno))
+            else:
+                # Relative import, combine the package name and module
+                # Note that module could be None
+                package = Path(path)
+                for _ in range(entity.level):
+                    package = package.parent
+                package = package.name
+                if entity.module:
+                    package = package + "." + entity.module
+                found[package].add((path, entity.lineno))
 
 
 def main():
@@ -38,14 +57,8 @@ def main():
     for path in paths:
         find_imports(path, found)
 
-#    with open("/tmp/imports.json", "w") as stream:
-#        import json
-#        json.dump(found, stream, indent=4)
-    import pprint
-    pprint.pprint(found)
-    breakpoint()
     for imported, imports in sorted(found.items()):
-        print(imported)
+        print(f"{imported}({len(imports)})")
         for path, lineno in sorted(imports):
             print(f"  {path}:{lineno}")
 
