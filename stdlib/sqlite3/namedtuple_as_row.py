b42e80c2f4cb2d8b67624496f1d5f86fbecb83e4
Hai Vu <haivu2004@gmail.com>
2025-03-29 08:03:33 -0700

    update namedtuple_as_row.py

--- stdlib/sqlite3/namedtuple_as_row.py
+++ stdlib/sqlite3/namedtuple_as_row.py
@@ -1,61 +1,67 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """
 Uses namedtuple as a row
 """
+
 import collections
 import sqlite3
 
-
 User = collections.namedtuple("User", ["uid", "alias", "shell"])
 Group = collections.namedtuple("Group", ["gid", "name"])
+ROWS_FACTORY = {type_._fields: type_._make for type_ in [User, Group]}
 
 
 def row_factory_func(cursor, row):
-	# cursor.description = [
-	#    ("uid", None, None, None, None, None, None),
-	#    ...
-	# ]
-	# See: https://docs.python.org/3.6/library/sqlite3.html#sqlite3.Cursor.description
+    fields = tuple(t[0] for t in cursor.description)
+    type_ = ROWS_FACTORY.get(fields, tuple)
+    return type_(row)
 
-	if cursor.description[0][0] == "uid":
-		return User._make(row)
-	elif cursor.description[0][0] == "gid":
-		return Group._make(row)
+
+def create_database(connection: sqlite3.Connection):
+    users = [
+        (501, "karen", "tcsh"),
+        (502, "john", "bash"),
+    ]
+
+    groups = [
+        (1001, "admin"),
+        (1002, "backup"),
+    ]
+
+    services = [
+        (901, "serviceA"),
+        (902, "serviceB"),
+    ]
+
+    connection.execute("create table user (uid int, alias text, shell text)")
+    connection.executemany("insert into user values (?, ?, ?)", users)
+
+    connection.execute("create table grp (gid int, name text)")
+    connection.executemany("insert into grp values (?, ?)", groups)
+
+    connection.execute("create table service (sid int, name text)")
+    connection.executemany("insert into service values (?, ?)", services)
+
+    connection.commit()
+    connection.row_factory = row_factory_func
 
 
 def main():
-	users = [
-		(501, "karen", "tcsh"),
-		(502, "john", "bash"),
-	]
+    with sqlite3.connect(":memory:") as connection:
+        create_database(connection)
 
-	groups = [
-		(1001, "admin"),
-		(1002, "backup"),
-	]
+        print("\n# Users")
+        for row in connection.execute("select * from user"):
+            print(row)
 
-	connection = sqlite3.connect(":memory:")
-	connection.execute("CREATE TABLE user (uid INT, alias TEXT, shell TEXT)")
-	connection.executemany("INSERT INTO user VALUES (?, ?, ?)", users)
-	connection.commit()
-	connection.execute("CREATE TABLE grp (gid INT, name TEXT)")
-	connection.executemany("INSERT INTO grp VALUES (?, ?)", groups)
-	connection.commit()
+        print("\n# Groups")
+        for row in connection.execute("select * from grp"):
+            print(row)
 
-	print("\nUser without setting row_factory")
-	sql = "SELECT * FROM user"
-	for row in connection.execute(sql):
-		print(row)
-
-	print("\nUser with setting row_factory")
-	connection.row_factory = row_factory_func
-	for row in connection.execute(sql):
-		print(row)
-
-	print("\nGroup with row_factory")
-	for row in connection.execute("SELECT * FROM grp"):
-		print(row)
+        print("\n# Services")
+        for row in connection.execute("select * from service"):
+            print(row)
 
 
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
