da5a18eb83836b6c233d3a5fe414cb44fd2b955e
Hai Vu <haivu2004@gmail.com>
2025-04-02 00:09:00 -0700

    Add target and game boards

--- topics/battleship/sandbox.py
+++ topics/battleship/sandbox.py
@@ -1,13 +1,9 @@
-from battleship_game import ShipBoard
+from battleship_game import GameBoard
 
-s = ShipBoard()
-s.add("A1", "A2", "A3", "A4", "A5")
-s.add("B3", "C3", "D3")
-s.add("G0", "H0")
-s.add("E6", "E7", "E8")
-s.add("F4", "G4", "H4", "I4")
-
-print(s)
-for co in "D1 D2 A6 A9 G0 H0 E9 E8 G0".split():
-    print(f"{co} --> {s.assess(co)}")
-print(s)
+b = GameBoard()
+b.ship.add("A1", "A2", "A3", "A4", "A5")
+b.ship.add("B3", "C3", "D3")
+b.ship.add("G0", "H0")
+b.ship.add("E6", "E7", "E8")
+b.ship.add("F4", "G4", "H4", "I4")
+print(b)
--- topics/battleship/src/battleship_game/target_board.py
+++ topics/battleship/src/battleship_game/target_board.py
@@ -4,40 +4,14 @@ from collections import defaultdict
 from . import const
 
 
-class ShipBoard:
+class TargetBoard:
     def __init__(self):
         self.grid = defaultdict(lambda: const.MARK_EMPTY)
-        self.health = {}
-        self.ship = {}
-        self.ids = iter(const.IDS)
         self.shots_count = 0
-
-    def add(self, *ship):
-        # TODO: Validate each coordinate, flag such coordinate as A13, or X2
-        # TODO: Validate adding ship to empty spaces
-        ship_id = next(self.ids)
-        for coord in ship:
-            self.grid[coord] = ship_id
-        self.ship[ship_id] = tuple(ship)
-        self.health[ship_id] = len(ship)
-
-    def assess(self, coord: str):
-        self.shots_count += 1
-        if self.grid[coord] == const.MARK_EMPTY:
-            self.grid[coord] = const.MARK_MISS
-        if self.grid[coord] in {const.MARK_HIT, const.MARK_MISS, const.MARK_SUNK}:
-            return self.grid[coord]
-
-        ship_id = self.grid[coord]
-        self.health[ship_id] -= 1
-        self.grid[coord] = (
-            const.MARK_SUNK if self.health[ship_id] == 0 else const.MARK_HIT
-        )
-        return self.grid[coord]
 
     def __str__(self):
         buf = io.StringIO()
-        buf.write("SHIPS".ljust(const.BOARD_WIDTH))
+        buf.write("TARGETS".ljust(const.BOARD_WIDTH))
         buf.write("\n\n")
         buf.write("  │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │\n")
         buf.write("──┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───│\n")
