07c35ac82e7877f427a42aa9de4450482050df14
Hai Vu <haivu2004@gmail.com>
2025-04-03 12:56:46 -0700

    Add Boyer-Moore pattern matching

--- topics/algorithms/pattern-matching/src/pattern_matching/__init__.py
+++ topics/algorithms/pattern-matching/src/pattern_matching/__init__.py
@@ -1,6 +1,3 @@
-from loguru import logger
-
-
 def jump_table(pattern: str):
     pattern = pattern
     table = {}
@@ -16,8 +13,6 @@ def jump_table(pattern: str):
         return table.get(ch, longest)
 
     return calculate_jump
-
-
 def match(src: str, pat: str) -> int:
     if pat == "":
         raise ValueError("Pattern cannot be empty")
@@ -28,16 +23,10 @@ def match(src: str, pat: str) -> int:
 
     while found_index < len(src):
         bad = src[found_index + longest - 1]
-        logger.debug(f"{found_index=}, {bad=}")
         for index in reversed(range(longest)):
             if src[found_index + index] != pat[index]:
-                logger.debug(
-                    f"mismatched at {src[found_index + index]=} and {pat[index]=}"
-                )
                 skip = jump(bad)
-                logger.debug(f"{skip=}")
                 found_index += skip
-                logger.debug(f"{found_index=}")
                 break
         else:
             return found_index
