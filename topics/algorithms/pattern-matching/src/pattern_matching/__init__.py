fe6e7c7afa333d4484e4531f608b3eee97782cd8
Hai Vu <haivu2004@gmail.com>
2025-04-03 13:20:15 -0700

    Add another round of practice for match()

--- topics/algorithms/pattern-matching/src/pattern_matching/__init__.py
+++ topics/algorithms/pattern-matching/src/pattern_matching/__init__.py
@@ -1,26 +1,20 @@
-def match(src: str, pat: str) -> int:
-    """
-    Boyer-Moore string matching algorithm
-    https://youtu.be/4Oj_ESzSNCk?si=JEmgLMcNkx5fjrm-
-    """
+def match(src, pat):
     if pat == "":
-        raise ValueError("Pattern cannot be empty")
+        raise ValueError()
 
-    # Create bad jump table
     pat_len = len(pat)
     src_len = len(src)
     table = {}
     for index, ch in enumerate(pat):
-        table[ch] = pat_len - index - 1
+        table[ch] = pat_len - 1 - index
     del table[ch]
 
-    # Scan/compre from right to left
-    found_index = 0
-    while (last_mismatch_index := found_index + pat_len - 1) < src_len:
+    found = 0
+    while (last := found + pat_len - 1) < src_len:
         for index in range(pat_len - 1, -1, -1):
-            if src[found_index + index] != pat[index]:
-                found_index += table.get(src[last_mismatch_index], pat_len)
+            if src[found + index] != pat[index]:
+                found += table.get(src[last], pat_len)
                 break
         else:
-            return found_index
-    raise ValueError(f"Pattern not found: {pat}")
+            return found
+    raise ValueError()
