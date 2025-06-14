6c47db0d129e302ded5809fa6ad22626132a003c
Hai Vu <haivu2004@gmail.com>
2025-06-14 08:10:07 -0700

    refactor: rename Car to Star
    - Calculate path in `__init__`
    - Use cached property

--- 3party/click/enum-choices/src/enum_choices/main.py
+++ 3party/click/enum-choices/src/enum_choices/main.py
@@ -1,36 +1,32 @@
 import enum
+import functools
 import pathlib
 
 import click
 import yaml
 
 
-class Car(enum.Enum):
-    """ "Cars that are named after celebrities."""
-
+class Star(enum.Enum):
     ELVIS = ("Elvis", "elvis.yaml")
     MARILYN = ("Marilyn", "marilyn.yaml")
-
-    # Aliases
-    MONROE = ("Monroe", "marilyn.yaml")
+    MONROE = ("Monroe", "marilyn.yaml")  # Same as Marilyn
 
     def __init__(self, value, path):
         self._value_ = value
-        self.path = path
+        self.path = pathlib.Path(__file__).parent / "config" / path
 
-    @property
+    @functools.cached_property
     def config(self):
-        path = pathlib.Path(__file__).parent / "config" / self.path
-        with open(path, "rb") as stream:
+        with open(self.path, "rb") as stream:
             return yaml.safe_load(stream)
 
 
 @click.command
 @click.option(
-    "-c", "--car", type=click.Choice(Car, case_sensitive=False), default=Car.ELVIS
+    "--star", type=click.Choice(Star, case_sensitive=False), default=Star.ELVIS
 )
-def main(car):
-    print(f"Nick Name: {car.name}")
-    print(f"Name: {car.value}")
-    print(f"Full Name: {car.config['Name']}")
-    print(f"Configuration: {car.config}")
+def main(star):
+    print(f"{star.name=}")
+    print(f"{star.value=}")
+    print(f"{star.path=}")
+    print(f"{star.config=}")
