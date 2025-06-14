f1d497dea0ff5b414bb2f717618e4f904b5b80c8
Hai Vu <haivu2004@gmail.com>
2025-06-13 22:59:12 -0700

    refactor: enum-choices shared config

---
+++ 3party/click/enum-choices/src/enum_choices/main.py
@@ -0,0 +1,35 @@
+import enum
+import pathlib
+
+import click
+import yaml
+
+
+class Car(enum.Enum):
+    """ "Cars that are named after celebrities."""
+
+    ELVIS = ("Elvis", "elvis.yaml")
+    MARILYN = ("Marilyn", "marilyn.yaml")
+
+    # Aliases
+    MONROE = ("Monroe", "marilyn.yaml")
+
+    def __init__(self, value, path):
+        self._value_ = value
+        self.path = path
+
+    @property
+    def config(self):
+        path = pathlib.Path(__file__).parent / "config" / self.path
+        with open(path, "rb") as stream:
+            return yaml.safe_load(stream)
+
+
+@click.command
+@click.option(
+    "-c", "--car", type=click.Choice(Car, case_sensitive=False), default=Car.ELVIS
+)
+def main(car):
+    print(f"Nick Name: {car.name}")
+    print(f"Name: {car.value}")
+    print(f"Full Name: {car.config['Name']}")
