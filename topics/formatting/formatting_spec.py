b5cfeea2e89d3055cc77dd2df57c506f2677304c
Hai Vu <haivu2004@gmail.com>
2025-06-16 21:59:26 -0700

    feat: Add formatting spec: csv

--- topics/formatting/formatting_spec.py
+++ topics/formatting/formatting_spec.py
@@ -1,5 +1,34 @@
 #!/usr/bin/env python3
 import dataclasses
+import logging
+import os
+import re
+
+logging.basicConfig(level=os.getenv("LOGLEVEL", "DEBUG"))
+logger = logging.getLogger(__name__)
+
+
+SPEC_PATTERN = re.compile(
+    r"""
+    ^             # Start
+    ([<>^])?      # One of: < > or ^
+    (\d*)?        # The width, a series of digits
+    (a|s|u|csv)?  # a=alias, s=shell, u=uid, csv=comma separated values
+    $             # End
+""",
+    re.VERBOSE,
+)
+
+
+def parse_spec(spec: str):
+    logger.debug("spec=%r", spec)
+    matched = SPEC_PATTERN.match(spec)
+    logging.debug("matched=%r", matched)
+    if matched:
+        logging.debug("groups=%r", matched.groups())
+
+    alignment, width, selector = matched.groups()
+    return alignment or "", width or "", selector or ""
 
 
 @dataclasses.dataclass
@@ -10,23 +39,19 @@ class User:
 
     def __format__(self, spec):
         """Spec: u: uid, a: alias, s: shell"""
-        if spec == "":
-            spec = "a"
-        if len(spec) == 1:
-            width = ""
-        else:
-            width, spec = spec[:-1], spec[-1]
-
-        if spec == "a":
+        alignment, width, selector = parse_spec(spec)
+        if selector == "a" or selector == "":
             value = self.alias
-        elif spec == "u":
+        elif selector == "u":
             value = self.uid
-        elif spec == "s":
+        elif selector == "s":
             value = self.shell
+        elif selector == "csv":
+            value = f"{self.uid},{self.alias},{self.shell}"
         else:
             raise ValueError(f"Invalid format spec: {spec!r}")
 
-        out = f"{value:{width}}"
+        out = f"{value:{alignment}{width}}"
         return out
 
 
@@ -35,10 +60,24 @@ def main():
     print("repr:", repr(user))
     print("-" * 20)
 
-    print(f"Spec=u: {user:>12u}")  # Right justify
-    print(f"Spec=a: {user:12a}")  # Left justify
-    print(f"Spec=:  {user}")  # No spec
-    print(f"Spec=s: {user:^12s}")  # Center
+    print("\n# No spec")
+    print("123456789012")
+    print(f"{user}")
+
+    print("\n# Spec='>12u'")
+    print("123456789012")
+    print(f"{user:>12u}")
+
+    print("\n# Spec='12a'")
+    print("123456789012")
+    print(f"{user:12a}")  # Left justify
+
+    print("\n# Spec='^12s'")
+    print("123456789012")
+    print(f"{user:^12s}")  # Center
+
+    print("\n# Spec='csv'")
+    print(f"{user:csv}")
 
 
 if __name__ == "__main__":
