b97be6bde8fd9dff331be3034d709af83213c287
Hai Vu <haivu2004@gmail.com>
2025-06-29 00:04:40 -0700

    update config-choice

--- 3party/click/config-choice/src/config_choice/__init__.py
+++ 3party/click/config-choice/src/config_choice/__init__.py
@@ -1,3 +1,4 @@
+import contextlib
 import dataclasses
 import functools
 import pathlib
@@ -22,22 +23,20 @@ class ConfigParam(click.Choice):
         root = (
             pathlib.Path(__file__).with_name("config").relative_to(pathlib.Path.cwd())
         )
-        self.config = {path.stem: path for path in root.glob("*.yaml")}
-        try:
-            aliases_file = self.config.pop("_aliases")
+        self.path = {path.stem: path for path in root.glob("*.yaml")}
+
+        with contextlib.suppress(KeyError):
+            aliases_file = self.path.pop("_aliases")
             with open(aliases_file, "rb") as stream:
                 self.aliases = yaml.safe_load(stream)
             for alias, name in self.aliases.items():
-                self.config[alias] = self.config[name]
-        except KeyError:
-            pass
+                self.path[alias] = self.path[name]
 
-        super().__init__(self.config, case_sensitive=False)
+        super().__init__(self.path, case_sensitive=False)
 
     def convert(self, value, param, ctx):
-        if value not in self.choices:
-            self.fail(f" {value} is not one of {', '.join(self.choices)}")
-        return ConfigFile(value, self.config[value])
+        value = super().convert(value, param, ctx)
+        return ConfigFile(value, self.path[value])
 
 
 @click.command
