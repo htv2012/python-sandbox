2c76e197082cce9dc1e979aaadcac72d7ccd2309
Hai Vu <haivu2004@gmail.com>
2025-06-29 07:17:43 -0700

    feat: Alter config file structure
    - Change config file structure to match with the ones at work
    - Add docstrings

--- 3party/click/config-choice/src/config_choice/config/venue_config.py
+++ 3party/click/config-choice/src/config_choice/config/venue_config.py
@@ -9,16 +9,35 @@ import yaml
 
 @dataclasses.dataclass(frozen=True)
 class ConfigFile:
-    value: str
+    """
+    Represent a venue configuration file, which consists of the following attributes
+
+    venue_id: Identify a venue, e.g. "devpit1"
+    path: the path to the configuration file
+    content: The parsed content of the file, a dictionary
+    """
+
+    venue_id: str
     path: pathlib.Path
 
     @functools.cached_property
     def content(self):
         with open(self.path, "rb") as stream:
-            return yaml.safe_load(stream)
+            return yaml.safe_load(stream)["venue_config"]
 
 
 class ConfigType(click.Choice):
+    """
+    A click custom type to gather all configuration files as choices.
+
+    If a configuration file is devpit1.yaml, then the choice for that
+    file is "devpit1". Among the configuration files, there is a special
+    _aliases.yaml file, which specifies all the aliases. For example,
+    treasureisland is an alias for devpit1. That means treasureisland
+    will use devpit1.yaml as its configuration file.
+
+    The click return type will be ConfigFile.
+    """
     def __init__(self):
         root = (
             pathlib.Path(__file__)
@@ -38,5 +57,9 @@ class ConfigType(click.Choice):
         super().__init__(self.path, case_sensitive=False)
 
     def convert(self, value, param, ctx):
+        # Call the base class' convert() to ensure the
+        # argument from command-line is valid
         value = super().convert(value, param, ctx)
+
+        # Convert from a string to ConfigFile
         return ConfigFile(value, self.path[value])
