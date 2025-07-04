8b77b3da2c7ccc416da9fa1fb3983548b4eb954b
Hai Vu <haivu2004@gmail.com>
2025-07-04 00:29:05 -0700

    Add color-by-keyword

---
+++ 3party/click/color-by-keyword/main.py
@@ -0,0 +1,17 @@
+import click
+
+
+def myecho(text: str):
+    text = text.replace("[PASSED]", click.style("[PASSED]", fg="bright_green"))
+    text = text.replace("[FAILED]", click.style("[FAILED]", fg="bright_red"))
+    text = text.replace("[WARNING]", click.style("[WARNING]", fg="bright_yellow"))
+    click.echo(text)
+
+def main():
+    myecho("test1 [PASSED]")
+    myecho("test2 [FAILED]")
+    myecho("test3 [WARNING]")
+
+
+if __name__ == "__main__":
+    main()
