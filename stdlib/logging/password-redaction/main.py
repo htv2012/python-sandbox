97693db80471e49ed63aae5e2b50220b9761297a
Hai Vu <haivu2004@gmail.com>
2025-07-26 08:21:39 -0700

    Add password-redaction

---
+++ stdlib/logging/password-redaction/main.py
@@ -0,0 +1,25 @@
+import logging
+import re
+
+
+def redact(logger):
+    def apply_redact(existing_formatter):
+        def do_format(record):
+            text = existing_formatter(record)
+            return re.sub(r"password=.*? ", "password=*** ", text)
+
+        return do_format
+
+    for handler in logger.handlers:
+        handler.formatter.format = apply_redact(handler.formatter.format)
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger()
+    redact(logger)
+    logger.info("My password=foo@bar123 Please do not lose it")
+
+
+if __name__ == "__main__":
+    main()
