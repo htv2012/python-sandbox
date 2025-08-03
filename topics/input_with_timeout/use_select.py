cfb18c0ac50e065156cf34cd208cc68dc40e7e12
Hai Vu <haivu2004@gmail.com>
2025-08-03 09:33:57 -0700

    feat: create timed_input()

--- topics/input_with_timeout/use_select.py
+++ topics/input_with_timeout/use_select.py
@@ -1,13 +1,19 @@
 import select
 import sys
 
-print("What is your name? ", end="")
-sys.stdout.flush()
-i, o, e = select.select([sys.stdin], [], [], 10)
+def timed_input(prompt, default, timeout: int = 10, show_default:bool=True):
+    sys.stdout.write(prompt)
+    if show_default:
+        sys.stdout.write(f"[{default}] ")
+    sys.stdout.flush()
+    stdin_list, _, _ = select.select([sys.stdin], [], [], timeout)
 
-if i:
-    name = sys.stdin.readline().strip()
-else:
-    name = "stranger"
+    if stdin_list:
+        answer = stdin_list[0].readline().rstrip()
+    else:
+        print()
+        answer = default
+    return answer or default
 
+name = timed_input("What is your name? ", default="stranger", timeout=5)
 print(f"Hello, {name}")
