00f250a01e4c8e0d5e940f85005220a8ee97ac4a
Hai Vu <haivu2004@gmail.com>
2025-08-12 22:12:09 -0700

    Update the format class for color_text.py

--- stdlib/logging/color_text.py
+++ stdlib/logging/color_text.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Demo: Colorize text using logging.Formatter."""
+
 import logging
 
 
@@ -16,18 +17,14 @@ class Colors:
     UNDERLINE = "\033[4m"
 
 
-kw_colors = {
-    "PID": Colors.YELLOW,
-    "Process Name": Colors.YELLOW,
-    "Hello": Colors.CYAN,
-    "Pass": Colors.GREEN,
-}
-
-
 class CustomFormatter(logging.Formatter):
+    def __init__(self, keyword_color, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.keyword_color = keyword_color
+
     def format(self, record):
         msg = record.msg
-        for kw, color in kw_colors.items():
+        for kw, color in self.keyword_color.items():
             msg = msg.replace(kw, f"{color}{kw}{Colors.RESET}")
         record.msg = msg
         out = super().format(record)
@@ -35,12 +32,19 @@ class CustomFormatter(logging.Formatter):
 
 
 def setup_logger():
+    color = {
+        "PID": Colors.YELLOW,
+        "Process Name": Colors.YELLOW,
+        "Hello": Colors.MAGENTA,
+        "Pass": Colors.GREEN,
+    }
+
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
 
     console_handler = logging.StreamHandler()
     console_formatter = CustomFormatter(
-        "%(filename)s(%(lineno)d) %(levelname)s %(message)s"
+        color, "%(filename)s(%(lineno)d) %(levelname)s %(message)s"
     )
     console_handler.setFormatter(console_formatter)
     logger.addHandler(console_handler)
@@ -48,8 +52,13 @@ def setup_logger():
     return logger
 
 
-logger = setup_logger()
-logger.info("Hello world")
-logger.info("PID: 33445")
-logger.info("Process Name: Hello")
-logger.info("Test result: Pass")
+def main():
+    logger = setup_logger()
+    logger.info("Hello world")
+    logger.debug("PID: 33445")
+    logger.info("Process Name: Hello")
+    logger.info("Test result: Pass")
+
+
+if __name__ == "__main__":
+    main()
