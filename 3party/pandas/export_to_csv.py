d9a8d8921131175cd65670f3b3a3c32441518025
Hai Vu <haivu2004@gmail.com>
2025-08-13 21:52:48 -0700

    Fix warning errors, reformat code

--- 3party/pandas/export_to_csv.py
+++ 3party/pandas/export_to_csv.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 
 def main():
-    data_frame = pd.read_json('raw.json')
-    data_frame.to_csv('raw.csv', index=False)
+    data_frame = pd.read_json("raw.json")
+    data_frame.to_csv("raw.csv", index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- 3party/pandas/tally_using_pandas.py
+++ 3party/pandas/tally_using_pandas.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
 import pandas as pd
-import numpy as np
 
-data_frame = pd.read_json('raw.json')
-groupped_by_name = data_frame.groupby('name')
 
-print('\nRAW')
-print(data_frame)
+def main():
+    data_frame = pd.read_json("raw.json")
+    groupped_by_name = data_frame.groupby("name")
 
-print('\nAGGREGATE')
-print(groupped_by_name.aggregate([np.sum, np.mean]))
+    print("\nRAW")
+    print(data_frame)
+
+    print("\nAGGREGATE")
+    # print(groupped_by_name.aggregate([np.sum, np.mean]))
+    print(groupped_by_name.aggregate(["count", "sum", "mean"]))
+
+
+if __name__ == "__main__":
+    main()
