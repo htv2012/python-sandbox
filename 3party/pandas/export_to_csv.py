02bad7084dd71857995206704f04d07afb148d70
Hai Vu <haivu2004@gmail.com>
2025-08-13 21:44:17 -0700

    update pandas

--- 3party/pandas/export_to_csv.py
+++ 3party/pandas/export_to_csv.py
@@ -3,6 +3,10 @@
 import pandas as pd
 
 
-if __name__ == '__main__':
+def main():
     data_frame = pd.read_json('raw.json')
     data_frame.to_csv('raw.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
