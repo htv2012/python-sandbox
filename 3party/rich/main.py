a2e9fb9e7489071bd2dcf280e966e2aabaa83d7f
Hai Vu <haivu2004@gmail.com>
2025-10-13 22:35:30 -0700

    Add rich

---
+++ 3party/rich/main.py
@@ -0,0 +1,10 @@
+import rich.console
+
+
+def main():
+    console = rich.console.Console(color_system="truecolor")
+    console.rule("[bold corn_flower_blue]Console Demo")
+
+
+if __name__ == "__main__":
+    main()
