2107cc482f30919a95eec945f7f963b15a5daf79
Hai Vu <haivu2004@gmail.com>
2025-10-19 09:42:43 -0700

    Complete the simulation

--- topics/tea-house/src/tea_house/main.py
+++ topics/tea-house/src/tea_house/main.py
@@ -9,20 +9,40 @@ from . import Order, load_items
 from .data import generate_names
 
 
-def take_order(names, items, que: queue.Queue):
-    for number in range(1, 21):
+def take_order(employees_count: int, names, items, que: queue.Queue):
+    for number in range(1, 7):
         time.sleep(random.random() * 2)
-        count = random.randint(1, 3)
+        count = random.randint(1, 4)
         order = Order(
             number=number, name=next(names), items=random.sample(items, count)
         )
+        que.put(order)
         logger.info(
-            f"New order #{order.number} from {order.name}: {', '.join(item.name for item in order.items)}"
+            f"Order #{order.number} from {order.name}: {', '.join(item.name for item in order.items)}"
         )
 
+    # These are sentinels with order number < 0 to signal end of
+    # queue. Each employee will receive one
+    for number in range(-employees_count, 0):
+        que.put(Order(number, "", []))
 
-def prepare_drink(que: queue.Queue):
-    pass
+
+def prepare_drink(employee, que: queue.Queue):
+    logger.info(f"{employee} start")
+
+    while True:
+        order = que.get()
+        logger.info(f"Order #{order.number} assigned to {employee}")
+        if order.number < 0:
+            que.task_done()
+            break
+        for item in order.items:
+            logger.info(f"Order #{order.number} {employee} prepares {item.name}")
+            time.sleep(random.randint(3, 4))
+        logger.info(f"Order #{order.number} is ready for customer {order.name}")
+        que.task_done()
+
+    logger.info(f"{employee} done")
 
 
 def main() -> None:
@@ -31,4 +51,9 @@ def main() -> None:
     customers_queue = queue.Queue()
 
     with ThreadPoolExecutor() as executor:
-        executor.submit(take_order, names, items, customers_queue)
+        for _ in range(2):
+            executor.submit(prepare_drink, employee=next(names), que=customers_queue)
+        executor.submit(
+            take_order, employees_count=3, names=names, items=items, que=customers_queue
+        )
+        customers_queue.join()
