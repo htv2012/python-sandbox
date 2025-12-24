72ea55ef9a43cafbde52c66c247afc72c42708b3
Hai Vu <haivu2004@gmail.com>
2025-12-24 15:27:29 -0800

    feat: Adding more data classes

--- topics/data-sizes-comparison/main.py
+++ topics/data-sizes-comparison/main.py
@@ -23,17 +23,39 @@ class UserDataclass:
     shell: str
 
 
-def info(obj):
-    print()
-    print(f"Object: {obj}")
-    print(f"size: {asizeof.asizeof(obj)}")
+@dataclasses.dataclass(slots=True)
+class UserDataclassWithSlots:
+    uid: int
+    alias: str
+    is_admin: bool
+    shell: str
+
+
+@dataclasses.dataclass(frozen=True)
+class UserDataclassFrozen:
+    uid: int
+    alias: str
+    is_admin: bool
+    shell: str
 
 
 users = [
     UserNamedTuple(501, "karenc", True, "/bin/bash"),
     UserNamedTuple2(501, "karenc", True, "/bin/bash"),
     UserDataclass(501, "karenc", True, "/bin/bash"),
+    UserDataclassFrozen(501, "karenc", True, "/bin/bash"),
+    UserDataclassWithSlots(501, "karenc", True, "/bin/bash"),
     types.SimpleNamespace(uid=501, alias="karenc", is_admin=True, shell="/bin/bash"),
 ]
+
+print("Size Object")
+print("---- ------")
 for user in users:
-    info(user)
+    print(f"{asizeof.asizeof(user):>4} {user}")
+
+print()
+print("Summary")
+print("- Named tuple from collections and typing are of the same size")
+print("- Named tuple is lighter weight than data classe")
+print("- Data class with slot is very light weight")
+print("- Adding frozen does not help reduce the size of a data class")
