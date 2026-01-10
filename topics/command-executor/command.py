5a9042196e7fbbbef9231673f53914cc9cfba8c4
Hai Vu <haivu2004@gmail.com>
2026-01-10 08:38:18 -0800

    chore: add more features to Response

--- topics/command-executor/command.py
+++ topics/command-executor/command.py
@@ -1,12 +1,40 @@
 #!/usr/bin/env python3
+import enum
 import shlex
 import subprocess
+
+
+class Status(enum.Enum):
+    UNKNOWN = enum.auto()
+    PENDING = enum.auto()
+    OK = enum.auto()
+    FAILED = enum.auto()
+    CANCELED = enum.auto()
 
 
 class Response:
     def __init__(self, proc: subprocess.Popen):
         self.proc = proc
-        self._state = None
+        self.code = None
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(status={self.status!r}, code={self.code!r})"
+
+    @property
+    def status(self) -> Status:
+        self.code = self.proc.poll()
+        if self.code is None:
+            return Status.PENDING
+        elif self.code < 0:
+            return Status.CANCELED
+        elif self.code == 0:
+            return Status.OK
+        else:
+            return Status.FAILED
+
+    def wait(self, timeout: int = -1):
+        self.proc.communicate(timeout=None if timeout == -1 else timeout)
+        return self.status
 
 
 class Exec:
--- topics/command-executor/main.py
+++ topics/command-executor/main.py
@@ -1,3 +1,10 @@
+from command import Exec
+
+e = Exec()
+r = e.run(["echo start; sleep 120; echo end"])
+print(r)
+
+
 def main():
     print("Hello from command-executor!")
 
