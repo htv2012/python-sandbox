7ea200592f93d2565ab1f6018cf892699697f73f
Hai Vu <haivu2004@gmail.com>
2026-02-04 09:54:45 -0800

    update dataclass-param2

---
+++ 3party/click/dataclass-param2/clicklib.py
@@ -0,0 +1,87 @@
+import contextlib
+import dataclasses
+import enum
+import functools
+from typing import Optional
+
+import click
+
+
+def create_option(prefix, field):
+    """Create a click option from a data class field."""
+    decl = f"--{prefix}-{field.name.replace('_', '-')}"
+    kwargs = {}
+
+    if field.default_factory in {list, set}:
+        kwargs["multiple"] = True
+        # (first arg to List[str] or Set[str], which is str)
+        kwargs["type"] = field.type.__args__[0]
+    elif field.type is bool:
+        kwargs["is_flag"] = True
+        decl = f"{decl}/--no-{decl.removeprefix('--')}"
+    elif issubclass(field.type, enum.Enum):
+        kwargs["type"] = click.Choice([e.value for e in field.type])
+    else:
+        kwargs["type"] = field.type
+
+    if (
+        field.default is dataclasses.MISSING
+        and field.default_factory is dataclasses.MISSING
+    ):
+        kwargs["required"] = True
+    elif field.default is not dataclasses.MISSING:
+        kwargs["default"] = field.default
+        kwargs["show_default"] = True
+
+    return click.option(decl, **kwargs)
+
+
+def convert(value, cast, cls):
+    if value is None:
+        return value
+    with contextlib.suppress(TypeError):
+        # issubclass might generate a TypeError
+        if issubclass(cls, enum.Enum):
+            return cls(value)
+    if cast is not dataclasses.MISSING:
+        return cast(value)
+    return value
+
+
+def dataclass_options(cls, name: Optional[str] = None):
+    """
+    Create options to instantiate and object of type cls.
+
+    Args:
+        cls: The class
+        name: The name to be passed into the decorated function.
+            This name is also the prefix --<name>- to all options.
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            nonlocal func, cls, options, name, casts
+
+            attrs = {}
+            for field, cast in zip(dataclasses.fields(cls), casts):
+                attrs[field.name] = kwargs.pop(f"{name}_{field.name}")
+                attrs[field.name] = convert(attrs[field.name], cast, field.type)
+
+            obj = cls(**attrs)
+            kwargs[name] = obj
+
+            return func(*args, **kwargs)
+
+        nonlocal cls, name
+
+        if name is None:
+            name = cls.__name__.lower()
+        options = [create_option(name, f) for f in dataclasses.fields(cls)]
+        casts = [f.default_factory for f in dataclasses.fields(cls)]
+        for opt in options:
+            wrapped = opt(wrapped)
+
+        return wrapped
+
+    return decorator
--- 3party/click/dataclass-param2/main.py
+++ 3party/click/dataclass-param2/main.py
@@ -1,62 +1,33 @@
 import dataclasses
+import enum
+from typing import List, Set
 
 import click
 
+from clicklib import dataclass_options
+
+
+class Department(enum.Enum):
+    ENGINEER = "engineer"
+    MARKETING = "marketing"
+
 
 @dataclasses.dataclass
-class User:
+class UserInfo:
     uid: int
     alias: str
-    admin: bool = False
+    department: Department
+    admin: bool = True
     security_score: float = 0.75
-    groups: list[str] = dataclasses.field(default_factory=list)
-
-
-for f in dataclasses.fields(User):
-    for name in "name type default default_factory init repr".split():
-        print(f"{name} = {getattr(f, name)}")
-    print()
-
-
-def create_option(prefix, field):
-    kwargs = dict(type=field.type)
-    if field.default is not dataclasses.MISSING:
-        kwargs["default"] = field.default
-
-    return click.option(f"--{prefix}-{field.name.replace('_', '-')}", **kwargs)
-
-
-def options(cls):
-    prefix = cls.__name__.lower()
-    _options = [create_option(prefix, f) for f in dataclasses.fields(cls)]
-
-    def pre_func(**kwargs):
-        nonlocal cls, _options, prefix
-        attrs = {}
-        for field in dataclasses.fields(cls):
-            attrs[field.name] = kwargs.pop(f"{prefix}_{field.name}")
-        obj = cls(**attrs)
-        kwargs[prefix] = obj
-        print(f"{kwargs=}")
-
-
-    def wrapped(func):
-        nonlocal pre_func
-        for opt in reversed(_options):
-            pre_func = opt(pre_func)
-        return pre_func
-
-    return wrapped
+    groups: List[str] = dataclasses.field(default_factory=list)
+    keys: Set[int] = dataclasses.field(default_factory=set)
 
 
 @click.command
-@options(User)
-def main(uid, alias, admin, security_score, groups):
-    print(f"{uid=}")
-    print(f"{alias=}")
-    print(f"{admin=}")
-    print(f"{security_score=}")
-    print(f"{groups=}")
+@dataclass_options(UserInfo, name="user")
+def main(user):
+    """Demo of how to construct a data class from command line."""
+    print(f"{user=}")
 
 
 if __name__ == "__main__":
