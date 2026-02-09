68055f5aba9cda7b03bba291f90d12c55eb1ecb9
Hai Vu <haivu2004@gmail.com>
2026-02-09 09:34:10 -0800

    feat: Update finally_tryout.py for different situations

--- topics/exception/finally_tryout.py
+++ topics/exception/finally_tryout.py
@@ -1,8 +1,41 @@
-def doit():
+#!/usr/bin/env python3
+def return_within_try():
     try:
-        return "Do it"
+        return "return value"
     finally:
-        print("Finally")
+        print("finally")
 
 
-print(doit())
+def return_within_except():
+    try:
+        raise TypeError("Out of coffee")
+    except TypeError as e:
+        print(repr(e))
+        return "error"
+    finally:
+        print("finally")
+
+
+def raise_within_try():
+    try:
+        raise ValueError("Out of milk")
+    finally:
+        print("finally")
+
+
+def main():
+    print("\n# return within try")
+    print(return_within_try())
+
+    print("\n# return within except")
+    print(return_within_except())
+
+    print("\n# raise within try")
+    try:
+        raise_within_try()
+    except Exception as error:
+        print(repr(error))
+
+
+if __name__ == "__main__":
+    main()
