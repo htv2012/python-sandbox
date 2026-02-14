ecb5ae0d35aaecf3623e3e45fd24cbcbf2a3fe3c
Hai Vu <haivu2004@gmail.com>
2026-02-14 11:03:36 -0800

    chore: Remove edit step

--- 3party/shellingham/main.py
+++ 3party/shellingham/main.py
@@ -4,8 +4,8 @@ import shellingham
 def main():
     shell_name, shell_path = shellingham.detect_shell()
     print("\n# Shellingham detects")
-    print(f"Shell name: {shell_name}")
-    print(f"Shell path: {shell_path}")
+    print(f"Shell name: {shell_name!r}")
+    print(f"Shell path: {shell_path!r}")
 
 
 if __name__ == "__main__":
