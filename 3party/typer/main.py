36330e938e00069ac16d6875ac5ab991065c42d4
Hai Vu <haivu2004@gmail.com>
2026-02-14 07:43:18 -0800

    feat: Add more typer param types

--- 3party/typer/main.py
+++ 3party/typer/main.py
@@ -1,13 +1,29 @@
+import enum
 from typing import Optional
 
 import typer
 
+app = typer.Typer()
 
-def main(name: str, formal: bool, intro: Optional[str] = None):
-    print(f"{name=}")
-    print(f"{formal=}")
-    print(f"{intro=}")
+
+class LogLevel(enum.Enum):
+    DEBUG = "debug"
+    INFO = "info"
+
+
+@app.command()
+def main(
+    name: str,
+    age: int,
+    duration: float,
+    formal: bool,
+    tags: list[str],
+    level: LogLevel,
+    intro: Optional[str] = None,
+):
+    for name, obj in locals().items():
+        print(f"{name}: {obj.__class__.__name__} = {obj!r}")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
