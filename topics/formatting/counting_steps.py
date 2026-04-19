fe7a4b804fb28df9533d28b46f7126f8ce0634e4
Hai Vu <haivu2004@gmail.com>
2026-04-19 11:04:50 -0700

    feat: add formatting to step counting in formatting

--- topics/formatting/counting_steps.py
+++ topics/formatting/counting_steps.py
@@ -4,10 +4,10 @@ Demo: Prints the steps with automatic step numbers
 """
 
 
-def steps_counter(number=1):
+def steps_counter(number=1, fmt="%2d. %s"):
     def step(message):
-        nonlocal number
-        result = "%2d. %s" % (number, message)
+        nonlocal number, fmt
+        result = fmt % (number, message)
         number += 1
         return result
 
@@ -16,7 +16,7 @@ def steps_counter(number=1):
 
 def main():
     """Entry"""
-    step = steps_counter()
+    step = steps_counter(fmt="%d) %s")
     print(step("Creating objects"))
     print(step("Query missing parameters"))
     print(step("Report"))
