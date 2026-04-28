1ff79304a238d624f22cd98bfdd03d988e529750
Hai Vu <haivu2004@gmail.com>
2026-04-28 16:03:49 -0700

    chore: Add more query

--- 3party/python-jsonpath/query.py
+++ 3party/python-jsonpath/query.py
@@ -3,11 +3,26 @@ import jsonpath
 import banner
 import data
 
-banner.banner("query unit price < 100")
-matches = jsonpath.query("$.products[?@.unit_price < 100]", data.inventory).select(
-    "unit_price", "name"
-)
-print("Unit Price  Name")
-print("----------  ----")
-for obj in matches:
-    print(f"{obj['unit_price']:>10}  {obj['name']}")
+
+def main():
+    banner.banner("Inexpensive Items")
+    matches = jsonpath.query("$.products[?@.unit_price < 100]", data.inventory).select(
+        "unit_price", "name"
+    )
+    print("Unit Price  Name")
+    print("----------  ----")
+    for obj in matches:
+        print(f"{obj['unit_price']:>10}  {obj['name']}")
+
+    banner.banner("Overstock")
+    matches = jsonpath.query(
+        "$.products[?@.inventory_count > 100]", data.inventory
+    ).select("inventory_count", "name")
+    print("In Stock  Name")
+    print("--------  ----")
+    for obj in matches:
+        print(f"{obj['inventory_count']:>8}  {obj['name']}")
+
+
+if __name__ == "__main__":
+    main()
