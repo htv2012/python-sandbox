de1d6cef0af9474f005dd2fa8c74af4c7fe90ff1
Hai Vu <haivu2004@gmail.com>
2026-04-28 22:26:24 -0700

    refactor: break main into smaller functions

--- 3party/python-jsonpath/query.py
+++ 3party/python-jsonpath/query.py
@@ -1,27 +1,39 @@
+import operator
+
 import jsonpath
 
 import banner
 import data
 
 
-def main():
+def show_inexpensive():
     banner.banner("Inexpensive Items")
-    matches = jsonpath.query("$.products[?@.unit_price < 100]", data.inventory).select(
-        "unit_price", "name"
-    )
+    expr = "$.products[?@.unit_price < 100]"
+    matches = jsonpath.query(expr, data.inventory).values()
     print("Unit Price  Name")
     print("----------  ----")
     for obj in matches:
-        print(f"{obj['unit_price']:>10}  {obj['name']}")
+        # print(f"{obj['unit_price']:>10}  {obj['name']}")
+        print("{unit_price:>10}  {name}".format(**obj))
 
+
+def show_overstock():
     banner.banner("Overstock")
-    matches = jsonpath.query(
-        "$.products[?@.inventory_count > 100]", data.inventory
-    ).select("inventory_count", "name")
+    expr = "$.products[?@.inventory_count > 100]"
+    matches = jsonpath.query(expr, data.inventory).select("inventory_count", "name")
+
+    # Since jsonpath does not provide sort, we will sort the result ourselves
+    items = sorted(matches, key=operator.itemgetter("inventory_count"), reverse=True)
+
     print("In Stock  Name")
     print("--------  ----")
-    for obj in matches:
-        print(f"{obj['inventory_count']:>8}  {obj['name']}")
+    for obj in items:
+        print("{inventory_count:>8}  {name}".format(**obj))
+
+
+def main():
+    show_inexpensive()
+    show_overstock()
 
 
 if __name__ == "__main__":
