cae8619271ec2cc09d269170d09053ff8841bdf9
Hai Vu <haivu2004@gmail.com>
2026-06-01 11:49:34 -0700

    feat: implement game using curses (#26)
    * feat: implement game using curses
    
    * feat: add pygame/game1
    
    * chore: move sandbox.py to curses_cli.py
    
    * chore: finish drawing the grid
    
    * chore: unfinish changes
    
    * chore: still in exploration stage
    
    * chore: replace Iterable with Iterator
    
    * chore: fine tune the screen
    
    * chore: left and right arrows work now
    
    * chore: game works now, has cursor bug
    
    * chore: add completion indicators
    
    * refactor: misc
    
    * chore: Move grid generation into generate.py
    
    * chore: use loguru for debugging
    
    * fix: push to full stack
    
    * chore: replace loguru with logging
    
    * chore: remove unused
    
    * chore: remove ty
    
    * feat: move all balls of the same color to a new column
    
    * chore: write a new log file each time

---
+++ topics/fun_and_games/sort-balls-cli/src/scb/asset.py
@@ -0,0 +1,12 @@
+import enum
+
+
+class Asset(enum.StrEnum):
+    EMPTY = "◼️"
+    CHECKED = "✅"
+    POP = "⬆"
+    PUSH = "⬇"
+
+    @classmethod
+    def balls(cls):
+        return ["🔴", "🟠", "🟡", "🟢", "🔵", "🟣", "🟤"]
