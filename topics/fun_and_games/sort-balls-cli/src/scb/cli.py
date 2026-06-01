8c9a929e57d0d680af89377cf9739a62a28a4442
Hai Vu <haivu2004@gmail.com>
2026-06-01 11:49:34 -0700

    feat: implement game using curses (#26)
    * feat: implement game using curses
    
    * feat: add pygame/game1
    
    * chore: move sandbox.py to curses_cli.py
    
    * chore: finish drawing the grid
    
    * chore: unfinish changes
    
    * chore: still in exploration stage
    
    * chore: replace Iterable with Iterator
    
    * chore: fine tune the screen
    
    * chore: left and right arrows work now
    
    * chore: game works now, has cursor bug
    
    * chore: add completion indicators
    
    * refactor: misc
    
    * chore: Move grid generation into generate.py
    
    * chore: use loguru for debugging
    
    * fix: push to full stack
    
    * chore: replace loguru with logging
    
    * chore: remove unused
    
    * chore: remove ty
    
    * feat: move all balls of the same color to a new column
    
    * chore: write a new log file each time

--- topics/fun_and_games/sort-balls-cli/src/scb/cli.py
+++ topics/fun_and_games/sort-balls-cli/src/scb/cli.py
@@ -1,74 +1,138 @@
+# sandbox.py
+import curses
 import enum
-import random
+import io
 
-from .draw import draw_grid
-from .grid import Grid
-from .stack import CAPACITY
-
-
-class Choice(enum.Enum):
-    MOVE = enum.auto()
-    QUIT = enum.auto()
-    REPEAT = enum.auto()
-    FILL = enum.auto()
+from .asset import Asset
+from .generate import create_grid
+from .grid import COLUMNS_COUNT, Grid
+from .logger import logger
+from .stack import EMPTY_VALUE
 
 
-class UserInput:
-    def __init__(self):
-        self.last = None
+class State(enum.Enum):
+    POP = enum.auto()
+    PUSH = enum.auto()
 
-    def get(self) -> tuple[Choice, tuple]:
-        while True:
-            try:
-                choice = input("> ")
-                if choice == "" and self.last:
-                    return self.last
-                elif choice == "q":
-                    return Choice.QUIT, tuple()
-                elif len(choice) == 2 and choice.isnumeric():
-                    self.last = Choice.MOVE, tuple(int(v) for v in choice)
-                    return self.last
-                elif len(choice) == 2 and choice[0] in "ropgybw":
-                    dest = int(choice[1])
-                    return Choice.FILL, (choice[0], dest)
-            except ValueError:
-                pass
+
+class Coord:
+    GRID_TOP_LEFT = (5, 1)
+    MESSAGE = (20, 1)
+
+
+class Action:
+    LEFT = ord("h")
+    RIGHT = ord("l")
+    TOGGLE = ord(" ")
+    QUIT = ord("q")
+
+
+def draw_grid(stdscr: curses.window, grid: Grid, ball: str, ball_column):
+    stdscr.clear()
+    stdscr.addstr(1, 1, "SORT COLOR BALLS")
+    y, x = Coord.GRID_TOP_LEFT
+
+    # Draw the "popped" ball
+    stdscr.addstr(y - 1, 3 + (ball_column * 5), ball)
+
+    for dy, row in enumerate(zip(*[stack.as_column for stack in grid])):
+        buf = io.StringIO()
+        buf.write("│ ")
+        buf.write(" │ ".join("◼️" if c == EMPTY_VALUE else c for c in row))
+        buf.write(" │")
+        stdscr.addstr(y + dy + 1, x, buf.getvalue())
+
+    # Draw the solved indicators
+    indicators = [
+        Asset.CHECKED if stack.is_completed else Asset.EMPTY for stack in grid
+    ]
+    stdscr.addstr(y + 9, x, "├────┼────┼────┼────┼────┼────┼────┼────┤")
+    stdscr.addstr(y + 10, x, "│ " + " │ ".join(indicators) + " │")
+
+    stdscr.refresh()
+
+
+def get_user_input(stdscr: curses.window, column: int, cursor: str):
+    y = 16
+    x = 3 + (column * 5)
+    logger.debug(f"get_user_input, {y=}, {x=}, {column=}")
+    stdscr.addstr(y, x, cursor)
+
+    while True:
+        key = stdscr.getch()
+        if key == Action.RIGHT:
+            logger.debug(f"Right from {y=}, {x=}, {column=}")
+            stdscr.addstr(y, x, " ")
+            column = (column + 1) % COLUMNS_COUNT
+            x = 3 + (column * 5)
+            stdscr.addstr(y, x, cursor)
+            logger.debug(f"        to {y=}, {x=}, {column=}")
+        elif key == Action.LEFT:
+            logger.debug(f"Left from {y=}, {x=}, {column=}")
+            stdscr.addstr(y, x, " ")
+            column = (column - 1) % COLUMNS_COUNT
+            x = 3 + (column * 5)
+            stdscr.addstr(y, x, cursor)
+            logger.debug(f"       to {y=}, {x=}, {column=}")
+        elif key == Action.TOGGLE:
+            logger.debug(f"Toggle, {column=}")
+            return Action.TOGGLE, column
+        elif key == Action.QUIT:
+            return Action.QUIT, -1
+        elif key == curses.KEY_MOUSE:
+            logger.debug("Mouse activity detected")
+            _, x_coord, y_coord, _, button_state = curses.getmouse()
+            if button_state & curses.BUTTON1_CLICKED:
+                clicked_column = (x_coord - 3) // 5
+                logger.debug(f"column {clicked_column} clicked")
+                if 0 <= clicked_column < COLUMNS_COUNT:
+                    return Action.TOGGLE, clicked_column
+
+
+def _main(stdscr: curses.window):
+    curses.curs_set(0)
+    curses.cbreak()
+    curses.noecho()
+    curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
+    stdscr.keypad(True)
+
+    grid = create_grid()
+    column = 0
+    state = State.POP
+    ball = " "
+    ball_column = column
+
+    while True:
+        draw_grid(stdscr, grid, ball, ball_column)
+        if grid.completed:
+            stdscr.addstr(*Coord.MESSAGE, "Sorted!")
+            stdscr.getch()
+            break
+        logger.debug(f"Before get_user_input, {column=}, {state=}, {ball=}")
+        action, column = get_user_input(
+            stdscr, column, Asset.POP if state == State.POP else Asset.PUSH
+        )
+        logger.debug(f"After get_user_input, {column=}, {action=}")
+        stack = grid[column]
+        if action == Action.QUIT:
+            break
+        elif action == Action.TOGGLE:
+            if state == State.POP:
+                if not stack.is_empty:
+                    ball = stack.pop()
+                    ball_column = column
+                    state = State.PUSH
+            elif state == State.PUSH:
+                logger.debug(f"Moving ball from column {ball_column} to {column}")
+                src = grid[ball_column]
+                src.push(ball)
+                while not stack.is_full and src.top == ball:
+                    top_ball = src.pop()
+                    stack.push(top_ball)
+                state = State.POP
+                ball = " "
+                ball_column = column
 
 
 def main():
-    grid = Grid()
-    user_input = UserInput()
-
-    balls = "".join(ch * 8 for ch in "🔴🟠🟡🟢🔵🟣🟤")
-    balls = list(balls)
-    random.shuffle(balls)
-    for i, ball in enumerate(balls):
-        stack_number = i // CAPACITY
-        grid.put(stack_number, ball)
-
-    while True:
-        draw_grid(grid)
-        choice, args = user_input.get()
-        if choice == Choice.QUIT:
-            break
-        elif choice == Choice.MOVE:
-            src, dest = args
-            try:
-                grid.move(from_stack=src, to_stack=dest)
-            except ValueError as err:
-                print(err)
-        elif choice == Choice.FILL:
-            picked, dest = args
-            for src, stack in enumerate(grid):
-                if src == dest:
-                    continue
-                while stack.top == picked:
-                    if grid[dest].is_full:
-                        break
-                    stack.pop()
-                    grid[dest].push(picked)
-
-        if grid.completed:
-            draw_grid(grid)
-            print("Sorted!")
-            break
+    curses.wrapper(_main)
---
+++ topics/fun_and_games/sort-balls-cli/src/scb/generate.py
@@ -0,0 +1,45 @@
+import collections
+import random
+
+from .asset import Asset
+from .grid import Grid
+from .stack import CAPACITY
+
+
+def diag_grid() -> Grid:
+    grid = Grid()
+    balls = collections.deque(Asset.balls())
+    for i in range(CAPACITY):
+        for stack, ball in zip(grid[:-1], balls):
+            stack.push(ball)
+        balls.rotate(1)
+
+    return grid
+
+
+def horizontal_grid() -> Grid:
+    grid = Grid()
+    balls = Asset.balls()
+    for stack in grid:
+        for ball in balls:
+            stack.push(ball)
+    return grid
+
+
+def random_grid() -> Grid:
+    grid = Grid()
+
+    balls = "".join(ch * 8 for ch in Asset.balls())
+    balls = list(balls)
+    random.shuffle(balls)
+    for i, ball in enumerate(balls):
+        stack_number = i // CAPACITY
+        grid.put(stack_number, ball)
+
+    return grid
+
+
+def create_grid() -> Grid:
+    grid_creators = [random_grid, horizontal_grid, diag_grid]
+    grid_creator = random.choice(grid_creators)
+    return grid_creator()
