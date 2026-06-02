cb5fe3cf8dad1aff4a39793b852362e03e6acec0
Hai Vu <haivu2004@gmail.com>
2026-06-02 11:11:09 -0700

    chore: adjust colors

--- topics/fun_and_games/sort-balls-cli/src/scb/colors.py
+++ topics/fun_and_games/sort-balls-cli/src/scb/colors.py
@@ -1,15 +1,14 @@
-BACKGROUND = (30, 30, 36)
-LINE = (150, 150, 150)  # Light gray for the dividers
-BALL_OUTLINE = (255, 255, 255)
+BACKGROUND = (48, 48, 48)
+LINE = (200, 200, 200)
+BALL_OUTLINE = (200, 200, 200)
 
 
-def balls():
-    return [
-        (235, 77, 75),  # red ball
-        (48, 144, 255),  # blue ball
-        (39, 174, 96),  # green ball
-        (241, 196, 15),  # yellow ball
-        (230, 126, 34),  # orange ball
-        (155, 89, 182),  # purple ball
-        (243, 104, 224),  # pink ball
-    ]
+BALLS = [
+    (235, 77, 75),  # red ball
+    (48, 144, 255),  # blue ball
+    (39, 174, 96),  # green ball
+    (241, 196, 15),  # yellow ball
+    (230, 126, 34),  # orange ball
+    (155, 89, 182),  # purple ball
+    (243, 104, 224),  # pink ball
+]
