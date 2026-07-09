d381e8384e2249de388bb7ba3eb6a6e38f8e01ae
Hai Vu <haivu2004@gmail.com>
2026-07-09 14:49:07 -0700

    chore: introduce ALL_INDICES to remove duplicates

--- topics/sudoku/src/sudoku/__init__.py
+++ topics/sudoku/src/sudoku/__init__.py
@@ -7,6 +7,7 @@ import re
 EMPTY_CELL = "."
 ROW_INDICES = list(range(9))
 COL_INDICES = list(range(9))
+ALL_INDICES = list(itertools.product(ROW_INDICES, COL_INDICES))
 
 
 class SudokuBoard:
@@ -29,7 +30,9 @@ class SudokuBoard:
         return buf.getvalue()
 
     def solve(self) -> bool:
-        for row, col in self.empty_cells():
+        for row, col in ALL_INDICES:
+            if self.board[row, col] != EMPTY_CELL:
+                continue
             for candidate in "123456789":
                 if self.conflicted(row, col, candidate):
                     continue
@@ -40,13 +43,20 @@ class SudokuBoard:
             return False
         return True
 
-    def conflicted(self, row, col, candidate):
+    def conflicted(self, row, col, candidate) -> bool:
+        """Does this candidate causes a conflict?"""
         return any(
             self.board[row2, col2] == candidate
             for row2, col2 in self.neighbors(row, col)
         )
 
     def neighbors(self, row, col):
+        """
+        Return the neighbors of a given cell.
+
+        This includes all the cells in the same row, all the cells
+        in the same column, and all the cells in the same block.
+        """
         yield from ((row, col2) for col2 in COL_INDICES if col != col2)
         yield from ((row2, col) for row2 in ROW_INDICES if row != row2)
 
@@ -57,32 +67,26 @@ class SudokuBoard:
                 if row != row2 and col != col2:
                     yield row2, col2
 
-    def empty_cells(self):
-        return (
-            (row, col)
-            for row, col in itertools.product(ROW_INDICES, COL_INDICES)
-            if self.board[row, col] == EMPTY_CELL
-        )
-
-    @classmethod
-    def from_grid(cls, grid):
-        me = cls()
-        for row_number, column in enumerate(grid):
-            for col_number, cell in enumerate(column):
-                me.board[row_number, col_number] = cell
-        return me
-
     @classmethod
     def from_sequence(cls, sequence):
+        """
+        Create a new Sudoku puzzle from a sequence of values.
+
+        The values can be '1' .. '9', and EMPTY_CELL.
+        """
         me = cls()
         seq = iter(sequence)
-        for row_number in ROW_INDICES:
-            for col_number in COL_INDICES:
-                me.board[row_number, col_number] = next(seq)
+        for row_number, col_number in ALL_INDICES:
+            me.board[row_number, col_number] = next(seq)
         return me
 
 
 def load(path: str | pathlib.Path):
+    """
+    Load (deserialize) a Sudoku puzzle.
+
+    Supports *.msk, *.sol, and *.ss formats.
+    """
     path = pathlib.Path(path)
     assert path.exists()
 
@@ -96,23 +100,23 @@ def load(path: str | pathlib.Path):
 
 
 def dump(puzzle: SudokuBoard, filename: str | pathlib.Path):
-    """Dump (serialize) a Sudoku puzzle, only support *.ss format."""
+    """Dump (serialize) a Sudoku puzzle in *.ss format."""
     path = pathlib.Path(filename)
     if path.suffix != ".ss":
         raise ValueError(f"Expect filename which ends with .ss, not {filename}")
 
     text = str(puzzle)
-    t = {
-        "┌": "*",
-        "┐": "*",
-        "└": "*",
-        "┘": "*",
-        "┬": "-",
-        "┴": "-",
-        "┼": "+",
-        "│": "|",
-        "─": "-",
-    }
-    for old, new in t.items():
+    replacement_table = [
+        ("┌", "*"),
+        ("┐", "*"),
+        ("└", "*"),
+        ("┘", "*"),
+        ("┬", "-"),
+        ("┴", "-"),
+        ("┼", "+"),
+        ("│", "|"),
+        ("─", "-"),
+    ]
+    for old, new in replacement_table:
         text = text.replace(old, new)
     path.write_text(text + "\n")
