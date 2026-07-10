9ee0ff1f1d2113d95f48dde46737523d7fd8e56f
Hai Vu <haivu2004@gmail.com>
2026-07-10 16:23:31 -0700

    feat: main.py to take a filename as argument

--- topics/sudoku/main.py
+++ topics/sudoku/main.py
@@ -1,11 +1,19 @@
+import argparse
+
 import sudoku
 
 
 def main():
-    puzzle = sudoku.load("puzzle1.ss")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    options = parser.parse_args()
+
+    puzzle = sudoku.load(options.filename)
     print(puzzle)
     if puzzle.solve():
         print(puzzle)
+    else:
+        print("Not solvable")
 
 
 if __name__ == "__main__":
