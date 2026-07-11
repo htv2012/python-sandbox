8eee19773199290e0125faf4799dbbc10d9b1e65
Hai Vu <haivu2004@gmail.com>
2026-07-11 00:09:17 -0700

    chore: add generate_ship

--- topics/battleship/src/battleship_game/player.py
+++ topics/battleship/src/battleship_game/player.py
@@ -1,6 +1,6 @@
 import random
 
-from .const import COLS, ROWS
+from .const import ALL_COORDINATES, COLS, ROWS, normalize_coordinate
 from .logger import logger
 from .ship_board import ShipBoard
 from .target_board import TargetBoard
@@ -12,6 +12,24 @@ class Player:
         self.ship_board = ShipBoard()
         self.target_board = TargetBoard()
 
+    def generate_ship(self, ship_id, ship_size) -> list[str]:
+        raise NotImplementedError("generate_ship")
+
+    def add_ships(self):
+        occupied = set()
+        for ship_id, ship_size in enumerate([3, 3, 4, 5, 2]):
+            conflicted = True
+            while conflicted:
+                ship = self.generate_ship(ship_id, ship_size)
+                conflicted = any(c in occupied for c in ship)
+
+                logger.debug("occupied: %r", occupied)
+                logger.debug("ship ID: %d, ship: %r", ship_id, ship)
+                logger.debug("conflicted: %r", conflicted)
+
+            self.ship_board.add(*ship)
+            occupied.update(ship)
+
     @property
     def is_lost(self) -> bool:
         return self.ship_board.all_sunk
@@ -22,29 +40,22 @@ class Player:
     def move(self):
         raise NotImplementedError()
 
-    def add(self):
-        raise NotImplementedError()
-
     def mark(self, coord, result):
         """Mark the target board."""
         raise NotImplementedError("mark")
 
 
 class HumanPlayer(Player):
-    def add_ships(self):
-        self.ship_board.add("A1", "A2", "A3")
-        self.ship_board.add("B1", "B2", "B3")
-        self.ship_board.add("C1", "C2", "C3", "C4")
-        self.ship_board.add("D1", "D2", "D3", "D4", "D5")
-        self.ship_board.add("E1", "E2")
+    def generate_ship(self, ship_id, ship_size) -> list[str]:
+        valid = False
+        while not valid:
+            ship = input(f"Ship #{ship_id} (size={ship_size}): ").upper().split()
+            ship = [normalize_coordinate(c) for c in ship]
+            valid = (len(ship) == ship_size) and all(c in ALL_COORDINATES for c in ship)
+        return ship
 
     def move(self):
         pass
-
-    def add(self):
-        for _ in range(5):
-            coordinates = input("Enter ship coordinates: ").upper().split()
-            self.ship_board.add(*coordinates)
 
     def mark(self, coord, result):
         """Mark the target board."""
@@ -55,31 +66,14 @@ class ComputerPlayer(Player):
     def move(self):
         pass
 
-    def add(self):
-        pass
-
-    def add_ships(self):
-        occupied = set()
-        for ship_id, ship_size in enumerate([3, 3, 4, 5, 2]):
-            conflicted = True
-            while conflicted:
-                direction = random.choice(["vertical", "horizontal"])
-                if direction == "vertical":
-                    row_index = random.randint(0, len(ROWS) - ship_size)
-                    col_index = random.randint(0, len(COLS) - 1)
-                    ship = [
-                        ROWS[row_index + i] + COLS[col_index] for i in range(ship_size)
-                    ]
-                else:
-                    row_index = random.randint(0, len(ROWS) - 1)
-                    col_index = random.randint(0, len(COLS) - ship_size)
-                    ship = [
-                        ROWS[row_index] + COLS[col_index + i] for i in range(ship_size)
-                    ]
-                conflicted = any(coord in occupied for coord in ship)
-                logger.debug("occupied: %r", occupied)
-                logger.debug(
-                    "ship ID: %d, ship: %r, conflicted: %r", ship_id, ship, conflicted
-                )
-
-            self.ship_board.add(*ship)
+    def generate_ship(self, ship_id, ship_size) -> list[str]:
+        direction = random.choice(["vertical", "horizontal"])
+        if direction == "vertical":
+            row_index = random.randint(0, len(ROWS) - ship_size)
+            col_index = random.randint(0, len(COLS) - 1)
+            ship = [ROWS[row_index + i] + COLS[col_index] for i in range(ship_size)]
+        else:
+            row_index = random.randint(0, len(ROWS) - 1)
+            col_index = random.randint(0, len(COLS) - ship_size)
+            ship = [ROWS[row_index] + COLS[col_index + i] for i in range(ship_size)]
+        return ship
