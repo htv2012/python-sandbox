fc06bb5b245c79c5dd8214a109e493b5a7d2882a
Hai Vu <haivu2004@gmail.com>
2026-08-02 23:07:36 -0700

    chore: add ls command

--- topics/fountain_pens_database/src/pens/shell.py
+++ topics/fountain_pens_database/src/pens/shell.py
@@ -1,6 +1,12 @@
 """Interactive shell"""
 
+import argparse
 import cmd
+import shlex
+
+INTRODUCTION = """
+Welcome to my stationery listing. Type help for a list of commands.
+"""
 
 
 def show(df):
@@ -8,16 +14,26 @@ def show(df):
     df = df.reset_index(drop=True)
     print(df.to_string())
 
+
 class Shell(cmd.Cmd):
     prompt = "\n> "
+    intro = INTRODUCTION
 
     def __init__(self, df):
         super().__init__(self)
         self.df = df
 
     def do_pens(self, args_text):
-        print("under construction")
         df = self.df[self.df["Category"] == "Fountain Pen"]
+        show(df)
+
+    def do_ls(self, args_text):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-p", "--pens", default=False, action="store_true")
+        args = parser.parse_args(shlex.split(args_text))
+        df = self.df
+        if args.pens:
+            df = self.df[self.df["Category"] == "Fountain Pen"]
         show(df)
 
     def emptyline(self):
