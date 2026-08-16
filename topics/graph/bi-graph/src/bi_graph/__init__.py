9873adf389d775c024f6697c6e6a75f183043993
Hai Vu <haivu2004@gmail.com>
2026-08-15 22:37:54 -0700

    chore: organize

--- topics/graph/bi-graph/src/bi_graph/__init__.py
+++ topics/graph/bi-graph/src/bi_graph/__init__.py
@@ -1,27 +1,3 @@
-import collections
+from .bi_graph import BiGraph
 
-
-def normalize_edge(node1, node2):
-    if node1 < node2:
-        return node1, node2
-    else:
-        return node2, node1
-
-
-class BiGraph:
-    def __init__(self):
-        self.graph = collections.defaultdict(set)
-        self.edges = set()
-        self.nodes = set()
-
-    def add_edge(self, node1, node2):
-        edge = normalize_edge(node1, node2)
-        if edge in self.edges:
-            return
-
-        self.edges.add(edge)
-        self.nodes.add(node1)
-        self.nodes.add(node2)
-
-        self.graph[node1].add(node2)
-        self.graph[node2].add(node1)
+__all__ = ["BiGraph"]
